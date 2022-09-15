6fa76d252df96aebb159fc6d3cd1b8f242f285da
Diego Siqueira <diego.camomila@gmail.com>
2022-09-15 14:51:54 -0300

    feat: .env

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,11 +1,13 @@
-import mongoose from 'mongoose';
-import 'dotenv/config';
+// import mongoose from 'mongoose';
+// import 'dotenv/config';
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/CarShop';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/CarShop';
 
-const connectToDatabase = (
-  mongoDatabaseURI = process.env.MONGO_URI
-    || MONGO_DB_URL,
-) => mongoose.connect(mongoDatabaseURI);
+// const connectToDatabase = (
+//   mongoDatabaseURI = process.env.MONGO_URI
+//     || MONGO_DB_URL,
+// ) => mongoose.connect(mongoDatabaseURI);
 
-export default connectToDatabase;
+// export default connectToDatabase;
+ 
+// coloquei a conection para fora nao entendi pq ja estava aqui.
