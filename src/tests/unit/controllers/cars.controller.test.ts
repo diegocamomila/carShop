66595821f12ec30c8c54ab0c99ab65d2431f5a37
Diego Siqueira <diego.camomila@gmail.com>
2022-09-18 11:57:50 -0300

    chore: testes camada controller funçao create

---
+++ src/tests/unit/controllers/cars.controller.test.ts
@@ -0,0 +1,37 @@
+import * as sinon from 'sinon';
+import chai from 'chai';
+const { expect } = chai;
+import CarsModel from '../../../models/cars.model';
+import CarsService from '../../../services/cars.service';
+import CarsController from '../../../controllers/cars.controller';
+import { resCreate } from '../../mocks/cars.mock';
+import { Request, Response } from 'express';
+
+const model = new CarsModel();
+const service = new CarsService(model);
+const controller = new CarsController(service);
+
+describe('Testes da camada controller cars.controller', () => {
+  const req = {} as Request;
+  const res = {} as Response;
+
+  describe('Em caso de sucesso', () => {
+    before(async () => {
+      sinon.stub(service, 'create').resolves(resCreate);
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+    });
+
+    after(()=>{
+      sinon.restore();
+    })
+
+    it('Testa a funçao create', async () => {
+      const response = await controller.create(req, res);
+
+      expect((response.status as sinon.SinonStub).calledWith(201)).to.be.true;
+      expect((response.json as sinon.SinonStub).calledWith(resCreate)).to.be.true;
+    });
+  });
+});
