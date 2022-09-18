5025a1995d0f6bdca75c920d5305b3bb026a7a58
Diego Siqueira <diego.camomila@gmail.com>
2022-09-18 18:36:26 -0300

    feat: iniciado rotas update e delete

--- src/routes/cars.route.ts
+++ src/routes/cars.route.ts
@@ -10,5 +10,6 @@ const routes = Router();
 routes.get('/:id', validateId, (req, res) => CarsController.readOne(req, res));
 routes.get('/', (req, res) => CarsController.read(req, res));
 routes.post('/', validateCar, (req, res) => CarsController.create(req, res));
-
+routes.put('/:id', validateId, validateCar, (req, res) => CarsController.update(req, res));
+routes.delete('/:id', validateId, (req, res) => CarsController.delete(req, res));
 export default routes;
